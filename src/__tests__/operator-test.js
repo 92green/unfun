1161dd0cecd53d076321918628700b47cd3a1474
Allan Hortle <allanhortle@gmail.com>
2018-10-29 17:47:45 +1100

    add: basic operators

---
+++ src/__tests__/operator-test.js
@@ -0,0 +1,98 @@
+//@flow
+import {add} from '../operator';
+import {subtract} from '../operator';
+import {multiply} from '../operator';
+import {divide} from '../operator';
+import {power} from '../operator';
+import {equals} from '../operator';
+import {notEquals} from '../operator';
+import {abstractEquals} from '../operator';
+import {abstractNotEquals} from '../operator';
+import {gt} from '../operator';
+import {gte} from '../operator';
+import {lt} from '../operator';
+import {lte} from '../operator';
+import {not} from '../operator';
+
+
+test('add', () => {
+    expect(add(1)(2)).toBe(2 + 1);
+});
+
+test('subtract', () => {
+    expect(subtract(1)(2)).toBe(2 - 1);
+});
+
+test('multiply', () => {
+    expect(multiply(1)(2)).toBe(2 * 1);
+});
+
+test('divide', () => {
+    expect(divide(1)(2)).toBe(2 / 1);
+});
+
+test('power', () => {
+    expect(power(3)(2)).toBe(2 ** 3);
+});
+
+describe('equals', () => {
+    test.each([
+        [1,1],
+        [true, true],
+    ])(
+        'equals: %p === %p',
+        (aa, bb) => {
+            expect(equals(aa)(bb)).toBe(bb === aa);
+        }
+    );
+
+});
+
+
+test('notEquals', () => {
+    expect(notEquals(3)(2)).toBe(3 !== 2);
+    expect(notEquals(true)(false)).toBeTruthy();
+});
+
+test('abstractEquals', () => {
+    expect(abstractEquals(3)(3)).toBeTruthy();
+    expect(abstractEquals(true)('true')).toBeTruthy();
+});
+
+test('abstractNotEquals', () => {
+    expect(abstractNotEquals(2)(3)).toBeTruthy();
+    expect(abstractNotEquals(false)('true')).toBeTruthy();
+});
+
+test('gt', () => expect(gt(2)(3)).toBeTruthy());
+test('lt', () => expect(lt(3)(2)).toBeTruthy());
+
+//gte
+test.each([
+    [1,1, true],
+    [3,2, true],
+    [-1,-3, true],
+    [0,3, false],
+    [-1,3, false]
+])(
+    'gte: %p >= %p is %p',
+    (aa, bb, expected) => expect(gte(bb)(aa)).toBe(expected)
+);
+
+//lte
+test.each([
+    [1,1, true],
+    [2,3, true],
+    [-3,-1, true],
+    [3,0, false],
+    [3,-1, false]
+])(
+    'lte: %i <= %i is %p',
+    (aa, bb, expected) => expect(lte(bb)(aa)).toBe(expected)
+);
+
+test('not', () => {
+    expect(not()(true)).toBe(false);
+    expect(not()(false)).toBe(true);
+    expect(not()("asdas")).toBe(false);
+});
---
+++ src/operator.js
@@ -0,0 +1,16 @@
+// @flow
+
+export const add = (aa: number) => (bb: number): number => bb + aa;
+export const subtract = (aa: number) => (bb: number): number => bb - aa;
+export const multiply = (aa: number) => (bb: number): number => bb * aa;
+export const divide = (aa: number) => (bb: number): number => bb / aa;
+export const power = (aa: number) => (bb: number): number => bb ** aa;
+export const equals = (aa: any) => (bb: any): any => bb === aa;
+export const notEquals = (aa: any) => (bb: any): any => bb !== aa;
+export const abstractEquals = (aa: any) => (bb: any): any => aa + bb;
+export const abstractNotEquals = (aa: any) => (bb: any): any => aa + bb;
+export const gt = (aa: number) => (bb: number): boolean => bb > aa;
+export const gte = (aa: number) => (bb: number): boolean => bb >= aa;
+export const lt = (aa: number) => (bb: number): boolean => bb < aa;
+export const lte = (aa: number) => (bb: number): boolean => bb <= aa;
+export const not = () => (bb: any): boolean => !bb;
